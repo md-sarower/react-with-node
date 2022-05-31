19e06aad864379057c435a183d3f687ed35a7166
md-sarower <sarower9415@gmail.com>
2022-05-31 06:44:17 +0600

    created data and loaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+useEffect(() => {
+  fetch('http://localhost:5000/user/')
+  .then(res => res.json())
+  .then(data => setUsers(data))
+}, [])
+
   return (
     <div className="App">
-      <h1>My own data</h1>
+      <h2>My own data: {users.length}</h2>
+      <ul>
+        {users.map(user => <li key={user.id}>id: {user.id}; Name: {user.name}; Email: {user.email}</li>)}
+      </ul>
     </div>
   );
 }
